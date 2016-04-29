19417602032cf84bef76e81c10313b5e1fbf5841
Alan Dean Kemp <alandeankemp@gmail.com>
2016-04-29 13:26:27 +0100

    Changed typo for identification intent

---
+++ libsimprints/src/main/java/com/simprints/libsimprints/Constants.java
@@ -0,0 +1,21 @@
+package com.simprints.libsimprints;
+
+public class Constants {
+
+    final public static String SIMPRINTS_REGISTER_INTENT = "com.simprints.id.REGISTER";
+    final public static String SIMPRINTS_IDENTIFY_INTENT = "com.simprints.id.IDENTIFY";
+
+    final public static String SIMPRINTS_ID = "id";
+    final public static String SIMPRINTS_API_KEY = "apiKey";
+    final public static String SIMPRINTS_CALLING_PACKAGE = "packageName";
+    final public static String SIMPRINTS_GUID = "guid";
+
+    final public static String SIMPRINTS_REGISTRATION = "registration";
+    final public static String SIMPRINTS_IDENTIFICATIONS = "identification";
+
+    final public static int RIGHT_INDEX = 0;
+    final public static int RIGHT_THUMB = 1;
+    final public static int LEFT_THUMB = 2;
+    final public static int LEFT_INDEX = 3;
+
+}
