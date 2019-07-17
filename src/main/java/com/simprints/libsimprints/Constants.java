f757ca4d9533585e04980856096fde81ba97c507
Aashay Gaikwad <aashay@simprints.com>
2019-07-17 16:48:12 +0100

    Removed safetynet down and error constants

--- src/main/java/com/simprints/libsimprints/Constants.java
+++ src/main/java/com/simprints/libsimprints/Constants.java
@@ -58,8 +58,6 @@ public class Constants {
     final public static int SIMPRINTS_INVALID_PROJECT_ID = Activity.RESULT_FIRST_USER + 19;
     final public static int SIMPRINTS_DIFFERENT_PROJECT_ID = Activity.RESULT_FIRST_USER + 20;
     final public static int SIMPRINTS_DIFFERENT_USER_ID = Activity.RESULT_FIRST_USER + 21;
-    final public static int SIMPRINTS_SAFETYNET_DOWN = Activity.RESULT_FIRST_USER +22;
-    final public static int SIMPRINTS_SAFETYNET_ERROR = Activity.RESULT_FIRST_USER + 23;
 
     // Result extras
     final public static String SIMPRINTS_REGISTRATION = "registration";
