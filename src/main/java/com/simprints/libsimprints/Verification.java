42dc5683a563c76c6b55586837d4801bb2626124
Milen Marinov <milen.danchev.marinov@gmail.com>
2024-07-08 20:24:02 +0300

    [MS-488] Add optional isSuccessful field in Verification

--- src/main/java/com/simprints/libsimprints/Verification.java
+++ src/main/java/com/simprints/libsimprints/Verification.java
@@ -11,6 +11,7 @@ public class Verification implements Parcelable {
     private int confidence;
     private Tier tier;
     private String guid;
+    private Boolean isSuccessful;
 
 
     /**
@@ -27,16 +28,23 @@ public class Verification implements Parcelable {
      * @param tier       The tier score derived from the confidence
      * @param guid       Global unique id of the verified person
      */
-    public Verification(int confidence, @NonNull Tier tier, @NonNull String guid) {
+    public Verification(int confidence, @NonNull Tier tier, @NonNull String guid, Boolean isSuccessful) {
         this.confidence = confidence;
         this.tier = tier;
         this.guid = guid;
+        this.isSuccessful = isSuccessful;
     }
 
     protected Verification(Parcel in) {
         confidence = in.readInt();
         tier = Tier.values()[in.readInt()];
         guid = in.readString();
+        // Read the additional byte to check if isSuccessful is null
+        if (in.readByte() == 0) {
+            isSuccessful = null;
+        } else {
+            isSuccessful = in.readByte() != 0;
+        }
     }
 
     public static final Creator<Verification> CREATOR = new Creator<Verification>() {
@@ -61,6 +69,12 @@ public class Verification implements Parcelable {
         dest.writeInt(this.confidence);
         dest.writeInt(this.tier.ordinal());
         dest.writeString(guid);
+        // Write an additional byte to indicate if isSuccessful is null
+        dest.writeByte((byte) (isSuccessful == null ? 0 : 1));
+        // Only write the boolean value if isSuccessful is not null
+        if (isSuccessful != null) {
+            dest.writeByte((byte) (isSuccessful ? 1 : 0));
+        }
     }
 
     public float getConfidence() {
@@ -87,5 +101,13 @@ public class Verification implements Parcelable {
 
     public void setGuid(@NonNull String guid) {
         this.guid = guid;
+    }
+
+    public Boolean getIsSuccessful() {
+        return isSuccessful;
+    }
+
+    public void setIsSuccessful(Boolean isSuccessful) {
+        this.isSuccessful = isSuccessful;
     }
 }
