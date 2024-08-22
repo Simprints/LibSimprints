0e41224afbfc5a3f654f5b7fa8a35c0c35b3f4fe
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-08-22 11:00:15 +0300

    MS-628 Add key version list for SID to use in response mapper

---
+++ src/main/java/com/simprints/libsimprints/contracts/VersionsList.kt
@@ -0,0 +1,17 @@
+package com.simprints.libsimprints.contracts
+
+/**
+ * List of versions in which specific changes to external API were introduced.
+ *
+ * These constant are mainly for Simprints ID to determine the optimal
+ * response data format based on the calling app integration version.
+ */
+object VersionsList {
+
+    /**
+     * In this version Activity contracts were introduced with a new set of returned data classes.
+     * The new API uses JSON as marshalling format instead of parcelable to make
+     * adding and removing data fields simpler.
+     */
+    const val INITIAL_REWORK = 20240801
+}
